25c639b4f1e51cfc4efa0b51b9657ef189d16208
Beau Kinstler <git@beaukinstler.info>
2021-09-22 22:02:17 -0500

    change: implement memory and operating system info

--- src/system.cpp
+++ src/system.cpp
@@ -30,11 +30,17 @@ std::string System::Kernel() {
   return this->kernel_;
 }
 
-// TODO: Return the system's memory utilization
-float System::MemoryUtilization() { return 0.0; }
+// Done: Return the system's memory utilization
+float System::MemoryUtilization() {
+  float mu = LinuxParser::MemoryUtilization();
+  return mu / 100.0;
+}
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// Done: Return the operating system name
+std::string System::OperatingSystem() {
+  // return string();
+  return LinuxParser::OperatingSystem();
+}
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
