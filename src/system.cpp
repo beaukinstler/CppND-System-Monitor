d365b1eefbcc01f3e4589af1d2cfd66d4db49f51
Beau Kinstler <git@beaukinstler.info>
2021-09-26 21:35:56 -0500

    update comment

--- src/system.cpp
+++ src/system.cpp
@@ -45,7 +45,7 @@ std::string System::OperatingSystem() {
 // Done: Return the number of processes actively running on the system
 int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
-// TODO: Return the total number of processes on the system
+// Done: Return the total number of processes on the system
 int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
 // TODO: Return the number of seconds since the system started running
