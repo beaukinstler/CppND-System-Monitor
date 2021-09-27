f582b729b2e5c83f978271dd99fcb07443889cd9
Beau Kinstler <git@beaukinstler.info>
2021-09-26 21:37:13 -0500

    change: added functions for uptime

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -103,7 +103,20 @@ float LinuxParser::MemoryUtilization() {
 }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() {
+  // get first number from the /proc/uptime file
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  string line;
+
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+
+    linestream >> line;
+  }
+  long result =  std::stol(line); // string to long
+  return result;
+ }
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
