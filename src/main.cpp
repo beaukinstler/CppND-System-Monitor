54b87cddcb0965b764c33db1476df7881fcc38b3
Beau Kinstler <git@beaukinstler.info>
2021-10-23 15:57:01 -0500

    remove tests from main

--- src/main.cpp
+++ src/main.cpp
@@ -9,61 +9,61 @@
 using std::cout;
 
 int main() {
-  // testing
+  // // testing
 
-  // testing code given for PIDs
+  // // testing code given for PIDs
 
-  vector<int> pidList = LinuxParser::Pids();
-  assert(!pidList.empty());
+  // vector<int> pidList = LinuxParser::Pids();
+  // assert(!pidList.empty());
 
-  // testing linux parser GetPidStat
-  vector<string> pidstats = LinuxParser::GetPidStat(pidList.front());
-  assert(pidstats.size() > 0);
+  // // testing linux parser GetPidStat
+  // vector<string> pidstats = LinuxParser::GetPidStat(pidList.front());
+  // assert(pidstats.size() > 0);
 
-  for (auto pid : pidList) {
-    // testing LinuxParser::Command(int pid)
-    cout << (LinuxParser::Command(pid)) << "\n";
+  // for (auto pid : pidList) {
+  //   // testing LinuxParser::Command(int pid)
+  //   cout << (LinuxParser::Command(pid)) << "\n";
 
-    // testing  LinuxParser::Ram(int pid)
-    string temp = LinuxParser::Ram(pid);
-    assert(temp.length() > 0);
+  //   // testing  LinuxParser::Ram(int pid)
+  //   string temp = LinuxParser::Ram(pid);
+  //   assert(temp.length() > 0);
 
-    // test LinuxParser::GetValuePidProcStatus(pid, "Uid", 1)
-    temp = LinuxParser::GetValuePidProcStatus(pid, "Uid", 1);
-    // assert(temp.length() > 0);
+  //   // test LinuxParser::GetValuePidProcStatus(pid, "Uid", 1)
+  //   temp = LinuxParser::GetValuePidProcStatus(pid, "Uid", 1);
+  //   // assert(temp.length() > 0);
 
-    // create a process
-    Process p(pid);
-    // test the GetStat function
-    if((p.GetStat()).size() >= 0 ){
-      assert((p.GetStat()).size() >= 0);     
-    }
-    else{
-      cout << p.Command() << "\n";
-    }
+  //   // create a process
+  //   Process p(pid);
+  //   // test the GetStat function
+  //   if((p.GetStat()).size() >= 0 ){
+  //     assert((p.GetStat()).size() >= 0);     
+  //   }
+  //   else{
+  //     cout << p.Command() << "\n";
+  //   }
 
 
-    // assert(p.CpuUtilization() >= 0 );
-  }
+  //   // assert(p.CpuUtilization() >= 0 );
+  // }
 
-  // // testing Format::ElapsedTime
-  long timeinsec = 10040;
-  std::string result = Format::ElapsedTime(timeinsec);
-  assert(result == "02:47:20");
+  // // // testing Format::ElapsedTime
+  // long timeinsec = 10040;
+  // std::string result = Format::ElapsedTime(timeinsec);
+  // assert(result == "02:47:20");
 
-  // testing linux parser
-  int total_procs = LinuxParser::TotalProcesses();
-  assert(total_procs > 0);
+  // // testing linux parser
+  // int total_procs = LinuxParser::TotalProcesses();
+  // assert(total_procs > 0);
 
-  // testing
-  float memUsed = LinuxParser::MemoryUtilization();
-  assert(memUsed <= 100 && memUsed > 0.00);
+  // // testing
+  // float memUsed = LinuxParser::MemoryUtilization();
+  // assert(memUsed <= 100 && memUsed > 0.00);
 
-  // testing
-  std::string kernel = LinuxParser::Kernel();
-  assert(kernel.length() > 0);
+  // // testing
+  // std::string kernel = LinuxParser::Kernel();
+  // assert(kernel.length() > 0);
 
-  // end of testing
+  // // end of testing
 
   System system;
   NCursesDisplay::Display(system);
